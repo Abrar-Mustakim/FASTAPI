cabd3e140ad62671d76f44824274dec60dd82ced
ABRAR MUSTAKIM <sm.abrar.mustakim@g.bracu.ac.bd>
2023-04-16 00:53:27 +0600

    Get Request

--- working.py
+++ working.py
@@ -6,3 +6,18 @@ app = FastAPI()
 def home():
     #return {"Data":"Test"}
     return "Hello World"
+
+@app.get("/about")
+def about():
+    return {"Data":"about"}
+
+
+inventory = {
+    1 : {"Name": "Air Force 1",
+         "Price": 100,
+         "Brand": "Nike"}
+}
+
+@app.get("/get-item/{item_id}")
+def get_item(item_id: int):
+    return inventory[item_id]
