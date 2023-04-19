0488e78541efb26f2727f8b29fd48afffd1086a8
ABRAR MUSTAKIM <sm.abrar.mustakim@g.bracu.ac.bd>
2023-04-19 17:36:58 +0600

    Request Body and post method

--- working.py
+++ working.py
@@ -7,9 +7,7 @@ app = FastAPI()
 
 class Item(BaseModel):
     name : str
-
     price : float 
-
     brand : Optional[str] = None
 
 @app.get("/")
@@ -21,12 +19,14 @@ def home():
 def about():
     return {"Data":"about"}
 
-
+'''
 inventory = {
     1 : {"Name": "Air Force 1",
          "Price": 100,
          "Brand": "Nike"}
 }
+'''
+inventory = {}
 '''
 @app.get("/get-item/{item_id}")
 def get_item(item_id: int):
@@ -38,17 +38,30 @@ def get_item(item_id: int = Path(description="The ID of the Item you would like 
     return inventory[item_id]
 
 
-
+'''
 @app.get("/get-by-name/{item_id}")
 def get_item(*, item_id:int, Name: Optional[str] = None, test: int=None):
 
     for item_id in inventory:
-        if inventory[item_id]["Name"] == Name: 
+        if inventory[item_id].name == Name: 
+            return inventory[item_id]
+        
+    return {"Data" : "Not Found"}
+'''
+
+@app.get("/get-by-name/")
+def get_item(*, Name: Optional[str] = None, test: int=None):
+
+    for item_id in inventory:
+        if inventory[item_id].name == Name: 
             return inventory[item_id]
         
     return {"Data" : "Not Found"}
 
 
-@app.post("/create-item")
-def create_item(item : Item):
-    return {}
+@app.post("/create-item/{item_id}")
+def create_item(item_id:int, item : Item):
+    if item_id in inventory:
+        return "Error! The ID already exist"
+    inventory[item_id] = item
+    return inventory[item_id]
